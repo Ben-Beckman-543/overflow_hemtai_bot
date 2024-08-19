0dc195437186baeaedf7315336e8bf1b439ab2a0
I'm Sanji <filesharingbot78@gmail.com>
2024-08-19 11:41:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -34,7 +34,7 @@ async def start_command(client: Client, message):
                     "Jᴏɪɴ Cʜᴀɴɴᴇʟ 1", url=client.link_one),
                         InlineKeyboardButton(
                             " Jᴏɪɴ Cʜᴀɴɴᴇʟ 2", url=client.link_two)
-                          ],[ InlineKeyboardButton(" Jᴏɪɴ Cʜᴀɴɴᴇʟ 3", url="https://t.me/+hgVMxeazMNlhYjBl")]
+                          ],[ InlineKeyboardButton(" Jᴏɪɴ Cʜᴀɴɴᴇʟ 3", url="https://t.me/+i2D4rA5m_P4xYjVl")]
             ]
             try:
                 btn.append(
